aee3a1b155c6f0ebcd6827ecbb7104db7f7c6e1a
Siddhant Bansal <bansal.siddhant@gmail.com>
2026-04-21 19:11:56 -0400

    Update disclaimer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
 export const Footer = () => {
   return (
     <div className="text-center text-xs sm:text-sm py-3 md:py-4 bg-base-300">
-      Made with ❤️ by Siddhant Bansal
+      Made with ❤️ by Siddhant Bansal - No ads, no tracking, free and open
+      source. All your data is local.{" "}
+      <a
+        href="https://www.github.com/sidbans/act-advisor"
+        className="text-blue-500 hover:underline"
+      >
+        Source code
+      </a>
+      .
     </div>
   );
 };
